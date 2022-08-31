4467d93c03d0633885fecc88883e3d1f3770e363
jacobcheetham7gmailcom <112541306+jacobcheetham7gmailcom@users.noreply.github.com>
2022-08-31 17:09:29 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "star":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . 1 d 1 . . . . . . 
+. . . . . . d 1 1 d 1 . . . . . 
+. . . . . d 1 1 1 1 1 d . . . . 
+. . . . . . d 1 1 1 1 . . . . . 
+. . . . . . . 1 d 1 . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "fake sun":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 2 2 2 2 2 2 . . . . . . . 
+. . 2 5 5 5 5 5 2 . . . . . . . 
+. . 2 5 5 5 5 5 2 . . . . . . . 
+. . 2 5 5 5 5 5 2 . . . . . . . 
+. . 2 4 4 4 4 5 2 . . . . . . . 
+. . 2 2 2 2 2 4 2 . . . . . . . 
+. . 2 2 2 2 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
